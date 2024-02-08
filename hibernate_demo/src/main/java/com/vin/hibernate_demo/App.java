447e41fcdb66ef9edd1e438975836c3239d46f9d
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-02-08 23:52:09 +0530

    Add files via upload
    creating a hibernate ORM in java project

---
+++ hibernate_demo/src/main/java/com/vin/hibernate_demo/App.java
@@ -0,0 +1,28 @@
+package com.vin.hibernate_demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       First_Class fc= new First_Class();
+       fc.setId(1);
+       fc.setName("john");
+       fc.setMarks(91);
+       Configuration con = new Configuration().configure().addAnnotatedClass(First_Class.class);
+       SessionFactory sf= con.buildSessionFactory();
+       Session session = sf.openSession();
+       Transaction tx =session.beginTransaction();
+       
+       session.persist(tx);
+       tx.commit();
+    }
+}
